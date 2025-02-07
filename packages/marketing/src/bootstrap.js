d3e92b5a27085fd1b02ea4df4279953621c6bd4e
Gauri1809 <gauranshijohari18@gmail.com>
2025-02-07 20:12:57 +0530

    Added loader, , set initialPath, browserHostory for dev in isolation

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createMemoryHistory, createBrowserHistory } from 'history';
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultBrowserHistory, initialPath }) => {
+  const history = defaultBrowserHistory || createMemoryHistory({
+    initialEntries: [initialPath]
+  });
+  if(onNavigate) {
+    history.listen(onNavigate); // call onNavigate whenever mem route changes with location
+  }
+  
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathName})=>{
+      const { pathname } = history.location;
+      if(nextPathName !== pathname) {
+        history.push(nextPathName);
+      }
+    }
+  }
 };
 
 // If we are in development and in isolation,
@@ -13,7 +29,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultBrowserHistory: createBrowserHistory() });
   }
 }
 
