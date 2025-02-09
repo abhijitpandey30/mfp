604534c36181b30bdaf59254a5f1aace592895ef
Gauri1809 <gauranshijohari18@gmail.com>
2025-02-09 15:30:50 +0530

    Added dashboard vue app to mfp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,21 +1,30 @@
-import React, { lazy, Suspense } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-
+import { createBrowserHistory } from 'history';
 import Header from './components/Header';
 import Loader from "./components/Progress"
-import { useState } from 'react';
 const MarketingLazy = lazy(()=> import("./components/MarketingApp"));
 const AuthLazy = lazy(()=> import("./components/AuthApp"));
+const DashboardLazy = lazy(()=> import("./components/DashboardApp"));
 
 const  generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
+const history = createBrowserHistory()
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(()=>{
+    if(isSignedIn) {
+      history.push("/dashboard");
+    } 
+    if (!isSignedIn && history.location.pathname === '/dashboard')
+      history.push('/')
+  }, [isSignedIn])
   return (
-      <BrowserRouter>
+      <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
       <div>
           <Header onSignOut={()=> setIsSignedIn(false)} isSignedIn={isSignedIn}/>
@@ -27,6 +36,10 @@ export default () => {
               <Route path="/auth" >
                 <AuthLazy  onSignIn={()=> setIsSignedIn(true)}/>
               </Route>
+              <Route path = "/dashboard">
+                {/* {!isSignedIn && <Redirect to="/" />} */}
+                <DashboardLazy/>
+              </Route>
               <Route path="/" component={MarketingLazy}></Route>
             </Switch>
           </Suspense>
@@ -34,7 +47,7 @@ export default () => {
       </StylesProvider>
         
 
-      </BrowserRouter>
+      </Router>
       
   );
 };
